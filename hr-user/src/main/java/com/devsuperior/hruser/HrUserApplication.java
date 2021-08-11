a153c72edd45edfb6005924e2850122d7b63722a
VivianeMSiqueira <vickymsa@hotmail.com>
2021-08-10 22:53:39 -0300

    UserRepository, UserResource, Zuul config

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -2,6 +2,7 @@ package com.devsuperior.hruser;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +15,7 @@ public class HrUserApplication implements CommandLineRunner{
 	private BCryptPasswordEncoder passwordEncoder; 
 	
 	public static void main(String[] args) {
-		//SpringApplication.run(HrUserApplication.class, args);
+		SpringApplication.run(HrUserApplication.class, args);
 	}
 
 	@Override
